10d9e25413b2dcccd9e8bef45f0d0eb1cd1ed3bc
Shrushti-Kurve <shrushtikurve5@gmail.com>
2026-07-29 20:40:14 +0530

    api updated

--- frontend/src/api/predictionApi.js
+++ frontend/src/api/predictionApi.js
@@ -1,6 +1,6 @@
 import api from "./api";
 
-export const predictDisease = async (data) => {
-  const response = await api.post("/predict-disease", data);
+export const predictOutbreak = async (data) => {
+  const response = await api.post("/predict-outbreak", data);
   return response.data;
 };
